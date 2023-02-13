f33ae65b66693d989f16cf3b944731549ab4d67d
molodoysever <dpnofficial1@gmail.com>
2023-02-13 22:12:54 +0200

    spring-cafe

--- src/test/java/ua/lyashko/cafe/RunnerTest.java
+++ src/test/java/ua/lyashko/cafe/RunnerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import ua.lyashko.cafe.controllers.*;
 
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 public class RunnerTest {
 
     @Autowired
